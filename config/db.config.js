47fa685ef727602ba4e56a862218badb73281a1c
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-07-14 13:21:00 -0300

    Using docker to run app

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 function connect() {
-    const uri = "mongodb://localhost:27017/url_shortner";
-    mongoose.connect(uri, {
-        useNewUrlParser: true
-    });
+  const uri = 'mongodb://db:27017/url_shortner';
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+  });
 }
 
 module.exports = {
-    connect
-}
+  connect,
+};
