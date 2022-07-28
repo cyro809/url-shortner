29f40a97524ccfcdfc7f8d294133b480b0c0f1f7
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-07-28 10:24:55 -0300

    Redirecting to page after 5 seconds. Add label to create short url button

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -2,9 +2,12 @@ let timeleft = 5;
 const redirectTimer = setInterval(() => {
   if (timeleft <= 0) {
     clearInterval(redirectTimer);
-    document.getElementById('countdown-timer').innerHTML = 'Redirecting now';
+    document.getElementById('countdown-timer').innerHTML = 'now';
+    setTimeout(() => {
+      window.location.href = document.querySelector('#url-link');
+    }, 1000);
   } else {
-    document.getElementById('countdown-timer').innerHTML = `${timeleft} seconds`;
+    document.getElementById('countdown-timer').innerHTML = `in ${timeleft} seconds`;
   }
   timeleft -= 1;
 }, 1000);
