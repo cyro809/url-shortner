b0d891f8bd4c8d1ccb2bf805511baa99ef0a8b24
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-08-14 13:04:24 -0300

    Adds gulp task to build css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-const { src, dest } = require('gulp');
+const { src, dest, series } = require('gulp');
 const minify = require('gulp-minify');
+const concatCss = require('gulp-concat-css');
+const cssNano = require('gulp-cssnano');
+const sass = require('gulp-sass')(require('sass'));
+
+const STATIC_PATH = 'static/';
+const SRC_STATIC_PATH = 'src/static/';
 
 function minifyjs() {
   return src('src/static/js/*.js', { allowEmpty: true })
     .pipe(minify({ noSource: true }))
-    .pipe(dest('static/js'));
+    .pipe(dest(`${STATIC_PATH}js`));
 }
 
-exports.default = minifyjs;
+function minifycss() {
+  return src(`${SRC_STATIC_PATH}sass/styles.scss`)
+    .pipe(sass())
+    .pipe(concatCss('styles.css'))
+    .pipe(dest(`${STATIC_PATH}css`));
+}
+
+exports.default = series(minifyjs, minifycss);
