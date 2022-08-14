ecfb13a494662d1e2a709acc1ccaccee485213bc
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-08-14 13:24:18 -0300

    Watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-const { src, dest, series } = require('gulp');
+const gulp = require('gulp');
 const minify = require('gulp-minify');
-const concatCss = require('gulp-concat-css');
 const cssNano = require('gulp-cssnano');
 const sass = require('gulp-sass')(require('sass'));
 
@@ -8,16 +7,25 @@ const STATIC_PATH = 'static/';
 const SRC_STATIC_PATH = 'src/static/';
 
 function minifyjs() {
-  return src('src/static/js/*.js', { allowEmpty: true })
+  return gulp.src('src/static/js/*.js', { allowEmpty: true })
     .pipe(minify({ noSource: true }))
-    .pipe(dest(`${STATIC_PATH}js`));
+    .pipe(gulp.dest(`${STATIC_PATH}js`));
 }
 
 function minifycss() {
-  return src(`${SRC_STATIC_PATH}sass/styles.scss`)
+  return gulp.src(`${SRC_STATIC_PATH}sass/styles.scss`)
     .pipe(sass())
-    .pipe(concatCss('styles.css'))
-    .pipe(dest(`${STATIC_PATH}css`));
+    .pipe(cssNano('styles.css'))
+    .pipe(gulp.dest(`${STATIC_PATH}css`));
 }
 
-exports.default = series(minifyjs, minifycss);
+function build() {
+  return gulp.series(minifyjs, minifycss);
+}
+
+function watch() {
+  gulp.watch(['src/static/js/*.js', `${SRC_STATIC_PATH}sass/*.scss`], build());
+}
+
+exports.default = gulp.series(minifyjs, minifycss);
+exports.watch = watch;
